370a53498948a84cefbb6904c729f531fa1a9d91
Marina <marina.iabluchanskaya@gmail.com>
2022-08-12 13:53:50 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import ContactSection from "./components/ContactSection";
+import ContactList from "./components/ContactList";
 import Search from "./components/Search";
+
 function App() {
   return (
     <div className="App">
       <main>
           <Search/>
-          <ContactSection/>
+          <div>
+              <h1>Contacts:</h1>
+              <ContactList/>
+          </div>
       </main>
     </div>
   );
